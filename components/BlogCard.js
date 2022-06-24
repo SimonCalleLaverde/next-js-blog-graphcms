136b40c1f72a180f680663dfce74e3305415fb37
Simon Calle Laverde <kyverde88@gmail.com>
2022-06-23 23:44:45 -0500

    Displaying/Creating "BlogCards".
    - Printing rest of data in props and query.
    - Styling. Organizing/Cleaning styles a bit.
    
    -- Vid 36:23.

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -2,15 +2,30 @@ import Link from "next/link";
 import styles from "../styles/BlogCard.module.css";
 
 // BlogCard Component
-export default function BlogCard({ title, author, coverPhoto, datePublished, slug }) {
+export default function BlogCard({ coverPhoto, title, content, author, datePublished, slug }) {
 	return (
-		<div className={styles.card}>
+		<article className={styles.card}>
 			<Link href={"/posts/" + slug}>
 				<div className={styles.imgWrapper}>
 					{/* Could Use "Image" Component From Next.js */}
 					<img src={coverPhoto.url} alt=""/>
 				</div>
 			</Link>
-		</div>
+
+			<div className={styles.caption}>
+				<h2>{title}</h2>
+
+				<div className={styles.details}>
+					<div className={styles.author}>
+						<img src={author.avatar.url} alt=""/>
+						<h3>{author.name}</h3>
+					</div>
+
+					<div className={styles.date}>
+						<h3>{datePublished}</h3>
+					</div>
+				</div>
+			</div>
+		</article>
 	)
 };
